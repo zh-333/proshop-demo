10f11cd4838a25d87d11f7020c9f3d35803c4ea0
Chan Zi Hao <zi333hao@gmail.com>
2024-09-10 15:36:01 +0800

    part 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Container } from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
-    <h1>Welcome to ProShop</h1>
-  )
-}
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default App
