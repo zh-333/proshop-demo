8b5d20ad958c2e10a04daedb573f6a4bb62ebfd9
Chan Zi Hao <zi333hao@gmail.com>
2024-09-12 15:06:39 +0800

    part 3 (authentication done)

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,7 +2,7 @@ import asyncHandler from '../middleware/asyncHandler.js';
 import Product from '../models/productModel.js';
 
 // @desc    Fetch all products
-// @route   GET /api/products
+// @route   GET /api/products 
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
     const products = await Product.find({});
